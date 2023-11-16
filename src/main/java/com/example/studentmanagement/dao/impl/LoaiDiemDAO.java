cb780c27edca83eab34ca47e9752784875e8b9ff
Huỳnh Mai Cao Nhân <huynhnhandanang@gmail.com>
2023-11-17 00:13:23 +0700

    Add LoaiDiemService, LoaiDiemDAO, floder Database

---
+++ src/main/java/com/example/studentmanagement/dao/impl/LoaiDiemDAO.java
@@ -0,0 +1,165 @@
+package com.example.studentmanagement.dao.impl;
+
+import com.example.studentmanagement.dao.ILoaiDiemDAO;
+import com.example.studentmanagement.model.LoaiDiem;
+
+import javax.persistence.*;
+import java.sql.SQLException;
+import java.util.List;
+
+public class LoaiDiemDAO implements ILoaiDiemDAO {
+
+    @Override
+    public List<LoaiDiem> findAll() {
+        EntityManagerFactory entityManagerFactory = null;
+        EntityManager entityManager = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("StudentManagementX");
+            entityManager = entityManagerFactory.createEntityManager();
+
+            TypedQuery<LoaiDiem> query = entityManager.createQuery("SELECT ld FROM LoaiDiem ld", LoaiDiem.class);
+            List<LoaiDiem> loaiDiemList = query.getResultList();
+
+            return loaiDiemList;
+        } catch (Exception e1) {
+            return null;
+        } finally {
+            try {
+                if (entityManagerFactory != null) {
+                    entityManagerFactory.close();
+                }
+                if (entityManager != null) {
+                    entityManager.close();
+                }
+            } catch (Exception e2) {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public LoaiDiem findOne(String maLoaiDiem) {
+        EntityManagerFactory entityManagerFactory = null;
+        EntityManager entityManager = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("StudentManagementX");
+            entityManager = entityManagerFactory.createEntityManager();
+
+            LoaiDiem ld = entityManager.find(LoaiDiem.class, maLoaiDiem);
+
+            return ld;
+        } catch (Exception e1) {
+            System.out.println(e1.getMessage());
+            return null;
+        } finally {
+            try {
+                if (entityManagerFactory != null) {
+                    entityManagerFactory.close();
+                }
+                if (entityManager != null) {
+                    entityManager.close();
+                }
+            } catch (Exception e2) {
+                System.out.println(e2.getMessage());
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public String save(LoaiDiem loaiDiem) {
+        EntityManagerFactory entityManagerFactory = null;
+        EntityManager entityManager = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("StudentManagementX");
+            entityManager = entityManagerFactory.createEntityManager();
+
+            entityManager.getTransaction().begin();
+
+            LoaiDiem ld = new LoaiDiem();
+            ld.setMaLoaiDiem(loaiDiem.getMaLoaiDiem());
+            ld.setTenLoaiDiem(loaiDiem.getTenLoaiDiem());
+
+            entityManager.persist(ld);
+            entityManager.getTransaction().commit();
+
+            return loaiDiem.getMaLoaiDiem();
+        } catch (Exception e1) {
+            System.out.println(e1.getMessage());
+            return null;
+        } finally {
+            try {
+                if (entityManagerFactory != null) {
+                    entityManagerFactory.close();
+                }
+                if (entityManager != null) {
+                    entityManager.close();
+                }
+            } catch (Exception e2) {
+                System.out.println(e2.getMessage());
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public void update(LoaiDiem loaiDiem) {
+        EntityManagerFactory entityManagerFactory = null;
+        EntityManager entityManager = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("StudentManagementX");
+            entityManager = entityManagerFactory.createEntityManager();
+
+            entityManager.getTransaction().begin();
+
+            LoaiDiem ld = entityManager.find(LoaiDiem.class, loaiDiem.getMaLoaiDiem());
+            ld.setTenLoaiDiem(loaiDiem.getTenLoaiDiem());
+
+            entityManager.merge(ld);
+            entityManager.getTransaction().commit();
+        } catch (Exception e1) {
+            System.out.println(e1.getMessage());
+        } finally {
+            try {
+                if (entityManagerFactory != null) {
+                    entityManagerFactory.close();
+                }
+                if (entityManager != null) {
+                    entityManager.close();
+                }
+            } catch (Exception e2) {
+                System.out.println(e2.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void delete(String maLoaiDiem) {
+        EntityManagerFactory entityManagerFactory = null;
+        EntityManager entityManager = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("StudentManagementX");
+            entityManager = entityManagerFactory.createEntityManager();
+
+            entityManager.getTransaction().begin();
+
+            LoaiDiem ld = entityManager.find(LoaiDiem.class, maLoaiDiem);
+
+            entityManager.remove(ld);
+            entityManager.getTransaction().commit();
+        } catch (Exception e1) {
+            System.out.println(e1.getMessage());
+        } finally {
+            try {
+                if (entityManagerFactory != null) {
+                    entityManagerFactory.close();
+                }
+                if (entityManager != null) {
+                    entityManager.close();
+                }
+            } catch (Exception e2) {
+                System.out.println(e2.getMessage());
+            }
+        }
+    }
+}
