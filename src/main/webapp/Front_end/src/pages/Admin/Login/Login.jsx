eb9bcc8b6add82f572181eb44d820ad434b6bdc8
Trần Quang Huy <huy.com2003@gmail.com>
2024-01-11 22:56:23 +0700

    Update login( còn cái background nữa)

--- src/main/webapp/Front_end/src/pages/Admin/Login/Login.jsx
+++ src/main/webapp/Front_end/src/pages/Admin/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Button, Card } from 'antd';
+import { Form, Input, Button, Card, notification } from 'antd';
 import { useNavigate  } from 'react-router-dom';
 
 import './Login.scss';
@@ -32,7 +32,15 @@ const Login = () => {
                 }
             }
             else {
-                console.log('Tài khoản hoặc mật khẩu không chính xác')
+                console.log('Tài khoản hoặc mật khẩu không chính xác');
+                notification.open({
+                    icon: <i class='bx bxs-error-alt' style={{color:'#ff0105'}} ></i>,
+                    message: 'Đăng nhập thất bại',
+                    description:
+                        'Tài khoản hoặc mật khẩu không chính xác',
+                    duration: 0,
+                    placement:'topLeft',
+                });
             }
         });
     }
@@ -42,33 +50,36 @@ const Login = () => {
 
     return (
         <div className="login-container" style={{ backgroundImage }}>
-            <Card title="Login" className="login-card">
-                <Form name="login-form"
-                    onFinish={onFinish}
-                >
-                    <Form.Item
-                        label="Tài khoản"
-                        name="username"
-                        rules={[{ required: true, message: 'Vui lòng nhập tài khoản của bạn!' }]}
+            <div className='loginRightPlacement'>
+                <Card  className="login-card">
+                    <span style={{color: 'black', fontWeight:'bold', fontSize:'35px'}}>Đăng nhập</span>
+                    <hr style={{color: '#00C2FF'}}></hr>
+                    <Form name="login-form"
+                        onFinish={onFinish}
                     >
-                        <Input />
-                    </Form.Item>
+                        <label>Tài khoản:</label>
+                        <Form.Item
+                            name="username"
+                            rules={[{ required: true, message: 'Vui lòng nhập tài khoản của bạn!' }]}
+                        >
+                        <Input style={{height: '45px'}}/>
+                        </Form.Item>
+                        <label>Mật khẩu:</label>
+                        <Form.Item
+                            name="password"
+                            rules={[{ required: true, message: 'Vui lòng nhập mật khẩu của bạn' }]}
+                        >
+                        <Input.Password style={{height: '45px'}}    />
+                        </Form.Item>
 
-                    <Form.Item
-                        label="Mật khẩu"
-                        name="password"
-                        rules={[{ required: true, message: 'Vui lòng nhập mật khẩu của bạn' }]}
-                    >
-                        <Input.Password />
-                    </Form.Item>
-
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" >
-                            Đăng nhập
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
+                        <Form.Item >
+                            <Button className='LoginButtonMain' type="primary" htmlType="submit" >
+                                Đăng nhập
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </div>
         </div>
     );
 };
