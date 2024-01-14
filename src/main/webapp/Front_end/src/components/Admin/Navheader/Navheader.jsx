8dc51c0eea51ffafcde1071889f4598b2cc092b9
Trần Quang Huy <huy.com2003@gmail.com>
2024-01-14 23:01:31 +0700

    Navbar color

--- src/main/webapp/Front_end/src/components/Admin/Navheader/Navheader.jsx
+++ src/main/webapp/Front_end/src/components/Admin/Navheader/Navheader.jsx
@@ -2,9 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { adminPaths } from '../../../routes/AppRoutes';
+import './Navheader.scss'
 function NavheaderAdmin() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" >
+    <Navbar expand="lg"  class="navbar navbar-dark bg-primary">
       <Container>
         <Navbar.Brand href={adminPaths.major_faculty}>Admin</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
